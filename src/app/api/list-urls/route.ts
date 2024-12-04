ef0ce522aef2e7a2d7aad61bfa0d21df183f927a
Walaa VOILIDAS <walaavolidis@gmail.com>
2024-12-04 08:58:18 +0200

    remove zod

--- src/app/api/list-urls/route.ts
+++ src/app/api/list-urls/route.ts
@@ -1,14 +1,5 @@
 import prisma from "@/lib/prisma";
-import { z } from "zod";
 import { getUser } from "@/lib/get-user";
-
-const ZUrlSchema = z.object({
-  id: z.string(),
-  original: z.string(),
-  shortened: z.string(),
-  createdAt: z.string().datetime(),
-  userId: z.string(),
-});
 
 export async function GET() {
   const user = await getUser();
@@ -17,10 +8,5 @@ export async function GET() {
       userId: user.id,
     },
   });
-  const formattedResponse = response.map((url) => ({
-    ...url,
-    createdAt: url.createdAt.toISOString(),
-  }));
-  const validatedResponse = ZUrlSchema.array().parse(formattedResponse);
-  return Response.json(validatedResponse);
+  return Response.json(response);
 }
